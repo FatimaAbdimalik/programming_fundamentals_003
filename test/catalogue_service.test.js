6ccd0dbb9779161879c503555b647eccd9b0a917
Fatima Abdimalik <f.abdimalik@gmail.com>
2020-03-29 09:54:51 +0100

    answered some questions

--- test/catalogue_service.test.js
+++ test/catalogue_service.test.js
@@ -9,3 +9,22 @@ describe("catalogueService", () => {
     });
   });
 });
+describe("catalogueService.checkBookByTitle", () => {
+  test("returns true if the book exists", () => {
+    expect(
+      catalogueService.checkBookByTitle(
+        "The Assassination of Margaret Thatcher"
+      )
+    ).toBe(true);
+  });
+});
+describe("cataloguesService.countBooksByFirstLetter", () => {
+  test("return total number of books that begins with given letter", () => {
+    expect(catalogueService.countBooksByFirstLetter("W")).toBe(2);
+  });
+});
+describe("cataloguesService.getQuantity", () => {
+  test("return quantity of books that in stock", () => {
+    expect(catalogueService.getQuantity("A Place of Greater Safety")).toBe(11);
+  });
+});
